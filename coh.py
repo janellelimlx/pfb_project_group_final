c7bff3167afee58fa426fbc4b2c8b0ee2450f2c6
asphod31 <109946311+asphod31@users.noreply.github.com>
2022-08-04 17:43:52 +0800

    Update coh.py

--- coh.py
+++ coh.py
@@ -1,1 +1,10 @@
-
+import api,overheads,cash_on_hand,profit_loss
+# main.py will import each python file as modules
+def main():
+    forex = api.api_function()
+    overheads.overhead_function(forex)
+    # by using overhead_function(forex) the exchange rate will be imported to overheads so that the amount can be converted from USD to SGD.
+    cash_on_hand.coh_function(forex)
+    profit_loss.profitloss_function(forex)
+main()
+# the main.py will coordinate all the files so functions are executed in just one file (main.py)
